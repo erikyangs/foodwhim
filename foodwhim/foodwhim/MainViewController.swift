147164647baa7301fb2159dbb4b2d8496af8182d
Erik Yang <erikyang@airbears2-10-142-35-53.airbears2.1918.berkeley.edu>
2017-04-25 18:43:06 -0700

    updated Pods

--- foodwhim/foodwhim/MainViewController.swift
+++ foodwhim/foodwhim/MainViewController.swift
@@ -13,7 +13,17 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("running main view controller")
+        print("Loaded MainViewController")
+        
+        //BLACK TRANSPARENCY LAYER
+        // get your window screen size
+        let screenSize: CGRect = UIScreen.main.bounds
+        //create a new view with the same size
+        let overlay: UIView = UIView(frame: CGRect(x:0, y:0, width:screenSize.width, height:screenSize.height))
+        // change the background color to black and the opacity to 0.6
+        overlay.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5)
+        // add this new view to your main view
+        self.view.addSubview(overlay)
     }
 
     override func didReceiveMemoryWarning() {
